5f99ebf79b4f7f548ecf928a0d8ebb9eeb43c734
Clemens Schmid <clemens@nevrome.de>
2019-09-12 11:06:23 +0200

    work on a basic data preparation workflow for the project

---
+++ project_analysis_work_draft.R
@@ -0,0 +1,88 @@
+library(magrittr)
+
+# get first radiocarbon date selection
+raw_data <- c14bazAAR::get_all_dates() %>%
+  dplyr::filter(
+    sourcedb %in% c("CALPAL", "CONTEXT", "EUBAR", "EUROEVOL", "Palmisano", "RADON", "RADON-B") 
+  )
+
+# remove dates without coordinates
+raw_data_with_coords <- raw_data %>% dplyr::filter(
+  !is.na(lat) & !is.na(lon) 
+)
+
+# fix country names
+raw_data_fixed_countries <- raw_data_with_coords %>% c14bazAAR::finalize_country_name()
+
+# filter dates to only include dates from Italy and Switzerland
+raw_data_italy_switzerland <- raw_data_fixed_countries %>% dplyr::filter(country_final %in% c("Switzerland", "Italy"))
+
+# remove duplicates
+raw_data_without_duplicates <- raw_data_italy_switzerland %>% c14bazAAR::remove_duplicates(
+  preferences = c("Palmisano", "RADON", "RADON-B", "EUBAR", "CONTEXT", "EUROEVOL", "CALPAL")
+)
+
+# remove dates without age information
+raw_data_with_age <- raw_data_without_duplicates %>%
+  # remove dates without age
+  dplyr::filter(!is.na(c14age) & !is.na(c14std)) %>%
+  # remove dates outside of theoretical calibration range
+  dplyr::filter(!(c14age < 71) & !(c14age > 46401))
+
+# calibrate dates
+# Calibration with Bchron could be broken: https://github.com/andrewcparnell/Bchron/pull/12
+threshold <- (1 - 0.9545) / 2
+raw_data_calibrated <- raw_data_with_age %>%
+  dplyr::mutate(
+    # add list column with the age density distribution for every date
+    calage_density_distribution = Bchron::BchronCalibrate(
+      ages      = raw_data_with_age$c14age,
+      ageSds    = raw_data_with_age$c14std,
+      calCurves = rep("intcal13", nrow(raw_data_with_age)),
+      eps       = 1e-06
+    ) %>%
+      # transform BchronCalibrate result to a informative tibble
+      # this tibble includes the years, the density per year,
+      # the normalized density per year and the information,
+      # if this year is in the two_sigma range for the current date
+      pbapply::pblapply(
+        function(x) {
+          x$densities %>% cumsum -> a      # cumulated density
+          bottom <- x$ageGrid[which(a <= threshold) %>% max]
+          top <- x$ageGrid[which(a > 1-threshold) %>% min]
+          tibble::tibble(
+            age = x$ageGrid,
+            dens_dist = x$densities,
+            norm_dens = x$densities/max(x$densities),
+            two_sigma = x$ageGrid >= bottom & x$ageGrid <= top
+          )
+        }
+      )
+    )
+
+# transform calBP age to calBC
+bol <- 1950
+raw_data_calibrated$calage_density_distribution %<>% lapply(
+  function(x) {
+    x$age = -x$age + bol
+    return(x)
+  }
+)
+
+# remove dates outsite of the timeframe of interest
+raw_data_in_time_of_interest <- raw_data_calibrated %>%
+  dplyr::mutate(
+    in_time_of_interest =
+      purrr::map(calage_density_distribution, function(x){
+        any(
+          x$age >= -10000 &
+            x$age <= -200 &
+            x$two_sigma
+        )
+      }
+    )
+  ) %>%
+    dplyr::filter(
+      in_time_of_interest == TRUE
+    ) %>%
+    dplyr::select(-in_time_of_interest)
