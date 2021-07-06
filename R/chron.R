2387af472081a5788d4097cd234df0620cfa2ca0
Joe Roe <joe@joeroe.io>
2021-07-06 16:55:41 +0200

    Add chron_as_sf()
    With sf as an optional dependency.

--- R/chron.R
+++ R/chron.R
@@ -1,5 +1,6 @@
 # chron.R
-# chron_*; high-level functions for retrieving chronological data from XRONOS
+# chron_*; high-level functions for retrieving and working with chronological
+# data from XRONOS
 
 #' Get chronological data from XRONOS
 #'
@@ -43,5 +44,46 @@ chron_data <- function(..., .everything = NA) {
   }
   else {
     xronos_query(names(params), params)
+  }
+}
+
+#' Convert a table of chronological data to an sf object
+#'
+#' Adds a simple features geometry column (see [sf::st_sf]) to chronological
+#' data from XRONOS, using the latitude and longitude columns. Rows with missing
+#' or invalid coordinates will be dropped with a warning.
+#'
+#' @param x data.frame retrieved with [chron_data()].
+#' @param crs Desired coordinate reference system in a format understood by
+#'  [sf::st_crs()]. Defaults to latitude/longitude on the WGS84 ellipsoid
+#'  (EPSG:4326).
+#'
+#' @return
+#' An `sf` object.
+#'
+#' @export
+#'
+#' @examples
+#' x <- chron_data(country = "Switzerland")
+#' chron_as_sf(x)
+chron_as_sf <- function(x, crs = sf::st_crs(4626)) {
+  if (requireNamespace("sf", quietly = TRUE)) {
+    y <- x[!is.na(x$lng) & !is.na(x$lat),]
+    y <- sf::st_as_sf(y, coords = c("lng", "lat"), crs = 4326)
+
+    d <- nrow(x) - nrow(y)
+    if (d > 0) {
+      rlang::warn(paste("Dropped", d, "rows with missing or invalid coordinates."))
+    }
+
+    if (crs != sf::st_crs(4626)) {
+      y <- sf::st_transform(y, crs)
+    }
+
+    y
+  }
+  else {
+    rlang::abort("`chron_as_sf()` requires package `sf`.",
+                 class = "xronos_missing_package")
   }
 }
