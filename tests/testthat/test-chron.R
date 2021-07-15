c90b2c964aa030eb6e348acf928dde41d7a2ad9f
Joe Roe <joe@joeroe.io>
2021-07-15 16:19:50 +0200

    Add tests for chron_data()

--- tests/testthat/test-chron.R
+++ tests/testthat/test-chron.R
@@ -1,3 +1,29 @@
+
+# chron_data() ------------------------------------------------------------
+
+test_that("chron_data(...) correctly translates parameters to xronos_query()", {
+  x <- chron_data(country = "Switzerland", material = c("bone", "charcoal"))
+  y <- xronos_query(c("country", "material"), list("Switzerland", c("bone", "charcoal")))
+  expect_equal(x, y)
+})
+
+test_that("chron_data() prompts for confirmation if `.everything` isn't set", {
+  # Don't actually make requests here
+  mockery::stub(chron_data, "xronos_request", NULL)
+
+  expect_warning(chron_data(.everything = TRUE), regexp = NA)
+
+  # Simulate interactive mode and prompt
+  mockery::stub(chron_data, "interactive", TRUE)
+  mockery::stub(chron_data, "utils::askYesNo", TRUE)
+  expect_message(chron_data(), regexp = "everything")
+
+  mockery::stub(chron_data, "utils::askYesNo", FALSE)
+  expect_null(chron_data())
+})
+
+# chron_as_sf() -----------------------------------------------------------
+
 test_that("chron_as_sf() returns a valid sf object", {
   load("../testdata/xronos_ch.RData")
   x <- xronos_ch[!is.na(xronos_ch$lat) & !is.na(xronos_ch$lng),]
